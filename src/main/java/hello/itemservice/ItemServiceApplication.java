dac474ae79109777cccd7a7c403f5d824dc274c7
dbqudgns <dbqudgns4632@gmai.com>
2024-12-23 19:39:01 +0900

    2024/12/23 Spring_DB-2

---
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -0,0 +1,30 @@
+package hello.itemservice;
+
+import hello.itemservice.config.*;
+import hello.itemservice.repository.ItemRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+
+
+@Import(MemoryConfig.class)
+@SpringBootApplication(scanBasePackages = "hello.itemservice.web")
+public class ItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceApplication.class, args);
+	}
+
+	@Bean
+	@Profile("local") //프로필 : 로컬(나의 PC), 운영 환경, 테스트 실행 등등 다양한 환경에 따라서 다른 설정을 할 때 사용하는 정보이다.
+	/*
+	스프링 애플리케이션이 실행될 때,
+	현재 활성화된 프로필(ex : spring.profiles.active=local)에 따라 특정 빈만 초기화된다.
+	 */
+	public TestDataInit testDataInit(ItemRepository itemRepository) {
+		return new TestDataInit(itemRepository);
+	}
+
+}
