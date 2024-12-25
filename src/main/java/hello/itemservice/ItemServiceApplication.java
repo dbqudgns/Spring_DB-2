e7f889dff8d21ff29c586632476a20e8fe8f7743
dbqudgns <dbqudgns4632@gmai.com>
2024-12-25 18:03:58 +0900

    2024/12/25 Spring_DB-2

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,13 +2,17 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 
+@Slf4j
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
@@ -28,6 +32,23 @@ public class ItemServiceApplication {
 	 */
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
+	}
+
+	@Bean
+	@Profile("test") // 프로필이 "test"인 경우에만 데이터소스를 스프링 빈으로 등록한다.
+	//임베디드 데이터베이스 사용을 위한 DataSource 설정
+	public DataSource dataSource() {
+		log.info("메모리 데이터베이스 초기화");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+		/**
+		 * jdbc:h2:mem:db : 임베디드 모드로 동작하는 H2 데이터베이스를 사용할 수 있다.
+		 * DB_CLOSE_DELAY=-1 : 임베디드 모드에서 데이터베이스 커넥션 연결이 모두 끊어질 때 데이터베이스가 종료되지 않도록 설정
+		**/
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
 	}
 
 }
